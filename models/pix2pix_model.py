9155fb6fe5300d28910a73c89c1c235d4ff00f83
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-05-17 21:55:56 +0000

    Added a test cloning command

--- models/pix2pix_model.py
+++ models/pix2pix_model.py
@@ -1,6 +1,8 @@
 import torch
+from torchvision.utils import save_image
 from .base_model import BaseModel
 from . import networks
+from copy import deepcopy
 
 
 class Pix2PixModel(BaseModel):
@@ -87,6 +89,7 @@ class Pix2PixModel(BaseModel):
         """Run forward pass; called by both functions <optimize_parameters> and <test>."""
         self.fake_B = self.netG(self.real_A)  # G(A)
 
+
     def backward_D(self):
         """Calculate GAN loss for the discriminator"""
         # Fake; stop backprop to the generator by detaching fake_B
@@ -109,6 +112,9 @@ class Pix2PixModel(BaseModel):
         self.loss_G_GAN = self.criterionGAN(pred_fake, True)
         # Second, G(A) = B
         self.loss_G_L1 = self.criterionL1(self.fake_B, self.real_B) * self.opt.lambda_L1
+        
+        #test = self.fake_B.clone()
+
         # combine loss and calculate gradients
         self.loss_G = self.loss_G_GAN + self.loss_G_L1
         self.loss_G.backward()
